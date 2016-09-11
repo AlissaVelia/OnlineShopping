4036cde2c0882a0bd7b2b66b8e2bd09d4598683c
Alissa Velia Royhatul Jannah <alyssavelia2980@gmail.com>
2016-09-11 21:09:38 +0700

    3. Mengubah layout dengan menambahkan ScrollView 3.Menambahkan value Domisili , dan menambahkan spinner di mode Text activityMain 4. Validasi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4003/onlineshopping/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4003/onlineshopping/MainActivity.java
@@ -2,8 +2,24 @@ package id.sch.smktelkom_mlg.tugas01.xirpl4003.onlineshopping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    EditText etTelp;
+    RadioButton rbJNE;
+    RadioButton rbTIKI;
+    CheckBox cb1, cb2, cb3, cb4;
+    RadioGroup rgJS;
+    Button bOrder;
+    Spinner spDomisili;
+    TextView tvHasil1 , tvHasil2 , tvHasil3 , tvHasil4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
